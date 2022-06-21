0bad1c7256ca37ab3763045971dd871fd3802b5e
Mike DeVita <mike@devita.co>
2022-06-21 12:48:00 -0700

    added prod api url

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
   publicRuntimeConfig: {
       apiUrl: process.env.NODE_ENV === 'development'
           ? 'http://localhost:3000/api' // development api
-          : 'http://localhost:3000/api' // production api
+          : 'https://flightpad.devita.co/api' // production api
   },
   exportPathMap: function() {
       return {
